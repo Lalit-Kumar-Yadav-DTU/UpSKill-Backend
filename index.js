5e2c9c8b9b40a0676b73cc27624829ac7e214a33
Lalit Kumar Yadav <lalitkumaryadav14112003@gmail.com>
2025-04-12 04:50:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: 'https://ed-tech-platform-frontend.onrender.com',
+        origin: 'https://upskill-frontend-exu8.onrender.com',
         credentials: true
     })
 );
